e6e9c87e040123a182bad66b2553779275c70a3f
ichttt <tobias.hotz@hotmail.de>
2018-12-25 11:43:06 +0100

    Bump version

--- src/main/java/ichttt/mclang2json/Lang2JsonConverter.java
+++ src/main/java/ichttt/mclang2json/Lang2JsonConverter.java
@@ -10,7 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Lang2JsonConverter {
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     public enum FileParseResult {
         SUCCESS, NO_LANG_FILES, ABORT, ERRORS
     }
